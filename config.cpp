944fa0f093bf08e0a24d3f90c0b0e762f0f913a1
Eric Johnson <flanker56@hotmail.com>
2026-04-13 18:10:54 -0400

    Added new config

---
+++ config.cpp
@@ -0,0 +1,16 @@
+class CfgPatches
+{
+	class KJ_UH_60
+	{
+		author="Flanker562";
+		name="Arma 3 Beta - Aircraft";
+		url="https://discord.gg/HbPTn278f6";
+		requiredAddons[]=
+		{
+			"A3_Air_F","UH_60_C"
+		};
+		requiredVersion=0.1;
+		units[]={};
+		weapons[]={};
+	};
+};
